49d248fde4aa5955feb03347573de68ecad4ba5f
Karol <56819265+lilivin@users.noreply.github.com>
2020-04-10 15:34:21 +0200

    Add files via upload

---
+++ script/feedback.js
@@ -0,0 +1,18 @@
+﻿const section = document.getElementById("feedback");
+const form = section.querySelector("form");
+const sent = document.getElementById("feedback-sent");
+
+function formSubmit(event) {
+    event.preventDefault();
+    form.classList.add("sending");
+};
+
+function animation() {
+    section.style.display = "none";
+    sent.style.display = "block";
+};
+
+form.addEventListener("submit", formSubmit, false);
+
+form.addEventListener("animationend", animation, false);
+
